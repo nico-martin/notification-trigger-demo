0a5ce6affe0b8e84d8982a3e5ec766bf0b519ae3
nico_martin <mail@nicomartin.ch>
2020-03-01 12:47:21 +0100

    fixed syntax error and small UX improvements

--- full/application.js
+++ full/application.js
@@ -1,26 +1,26 @@
 const $button = document.querySelector('#notification-button');
-const $featureDetection = document.querySelector('#feature-detection');
+const $error = document.querySelector('#error');
 
-if (!('serviceWorker' in navigator) || !('showTrigger' in Notification.prototype)) {
-  $button.style.display = 'none';
-  $featureDetection.style.display = 'block';
-} else {
+try {
+  if (!('serviceWorker' in navigator) || !('showTrigger' in Notification.prototype)) {
+    throw 'ServiceWorker or Notification Triggers API is not supported'
+  }
 
   // register the ServiceWorker
   navigator.serviceWorker.register('service-worker.js');
 
   // listen to the message event.
   navigator.serviceWorker.addEventListener('message', async event => {
-    if (event.data?.type === 'notification-clicked' || event.data?.type === 'notification-closed') {
+    if (event.data.type === 'notification-clicked' || event.data.type === 'notification-closed') {
       console.log(
-        event.data?.type === 'notification-clicked'
+        event.data.type === 'notification-clicked'
           ? 'Notification "Demo Push Notification" clicked.'
           : 'Notification "Demo Push Notification" closed.'
       );
     }
   });
 
-  // c
+  // button click
   $button.onclick = async () => {
     const reg = await navigator.serviceWorker.getRegistration();
     Notification.requestPermission().then(permission => {
@@ -39,8 +39,14 @@ if (!('serviceWorker' in navigator) || !('showTrigger' in Notification.prototype
             },
             badge: './assets/badge.png',
             icon: './assets/icon.png',
-          });
+          }
+        );
+        $button.setAttribute('disabled', 'disabled');
       }
     });
   };
+} catch (e) {
+  $button.style.display = 'none';
+  $error.style.display = 'block';
+  $error.innerHTML = e;
 }
